3a0a6c9a54e8006354cb0fbb5876831cb59461ad
“Arpita <arpitakumari10653@gmail.com>
2025-05-29 21:40:13 +0530

    using of elif

--- 1_conditional.py
+++ 1_conditional.py
@@ -1,7 +1,17 @@
 a = int(input("Enter your age: "))
 
+# If elif else ladder
+
 if(a>=18):
     print("You are above the age on consent")
 
+elif(a<0):
+    print("you are entering an invalid negative age")
+
+elif(a==0):
+    print("ypu are entering 0 which is not a valid age")
+
 else:
     print("You are above the age on consent") 
+
+print("End of program")    
