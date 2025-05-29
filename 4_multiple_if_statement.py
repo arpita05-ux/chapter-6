3876a5b90c5424962f119ab0b38d5bf5ef4162f2
“Arpita <arpitakumari10653@gmail.com>
2025-05-29 22:17:27 +0530

    running multiple if statement is possible and run separately

---
+++ 4_multiple_if_statement.py
@@ -0,0 +1,19 @@
+a = int(input("Enter your age: "))
+
+# If statement no: 1
+if(a%2==0):
+    print("a is even")
+# End of if statement no: 1
+
+# If statement no: 2
+if(a>=18):
+    print("You are above the age on consent")
+
+elif(a<0):
+    print("you are entering an invalid negative age")
+
+else:
+    print("You are above the age on consent") 
+# End of statement 2
+
+print("End of program") 
